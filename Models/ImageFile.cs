778423a6970731570553ad9517991ab244bc27ea
IdoD <idodror10@gmail.com>
2017-12-20 16:20:16 +0200

    Changed name from MyFileObject.cs to ImageFile.cs

--- Models/ImageFile.cs
+++ Models/ImageFile.cs
@@ -1,20 +1,20 @@
 namespace CloudProject.Models
 {
-    public class MyFile
+    public class ImageFile
     {
         public string _id { get; set; }    // file name
         public string _rev { get; set; }
-        public string filetype { get; set; }    // .doc .pdf .jpg .png etc...
+        public string filetype { get; set; }    // .jpg .png .gif etc...
         public string data { get; set; }    // file as Base64
     }
 
-    public class MyFileNoRev
+    public class ImageFileNoRev
     {
         public string _id { get; set; }    // file name
         public string filetype { get; set; }    // .doc .pdf .jpg .png etc...
         public string data { get; set; }    // file as Base64
 
-        public MyFileNoRev(MyFile file) {
+        public ImageFileNoRev(ImageFile file) {
             this._id = "id:" + file._id;
             this.filetype = file.filetype;
             this.data = file.data;
