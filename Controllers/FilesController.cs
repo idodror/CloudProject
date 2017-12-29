387b465689bbe4694d795fd50bb26b41d090a139
morisamon <amonmoris@gmail.com>
2017-12-29 19:34:17 +0200

    GetList in FilesContoller service

--- Controllers/FilesController.cs
+++ Controllers/FilesController.cs
@@ -6,9 +6,11 @@ using Microsoft.AspNetCore.Mvc;
 using CloudProject.Models;
 using System.Net.Http;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace CloudProject.Controllers
 {
+
     [Route("api/[controller]")]
     public class FilesController : Controller
     {
@@ -36,5 +38,28 @@ namespace CloudProject.Controllers
             
             return file;
         }
+
+
+        [HttpGet]
+        [Route("GetList/{id}")] //all image by id (user)
+        public async Task<IEnumerable<ImageFile>> GetList(string id) {
+            var hc = Helpers.CouchDBConnect.GetClient("files");
+
+            //var response = await hc.GetAsync("/files/_all_docs?startkey=%22file:" + id +"%22");
+            var response = await hc.GetAsync("/files/_all_docs?startkey=\"file:moris\"&include_docs=true");
+
+            if (!response.IsSuccessStatusCode) {
+                    return null;
+            }
+
+            var content = await response.Content.ReadAsStringAsync();
+            JObject json = JObject.Parse(content);
+
+            var filList = (IEnumerable<ImageFile>) JsonConvert.DeserializeObject(json.ToString(),typeof(IEnumerable<ImageFile>));
+            
+            return filList;
+        }
     }
-}
+      
+        
+}            
